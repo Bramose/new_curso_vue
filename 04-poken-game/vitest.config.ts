ca2c2c37781aab858c909d41606b9d5f17eb11f4
Sergio Bravo <bramose_123@hotmail.com>
2024-10-07 08:39:07 -0600

    En la aplicación de Pokemon Game, se usa la configuración Inicial de Vitest, pero se realizan los ajustes necesarios para que se pueda trabajar con una carpeta Global de Test

--- 04-poken-game/vitest.config.ts
+++ 04-poken-game/vitest.config.ts
@@ -6,6 +6,8 @@ export default mergeConfig(
   viteConfig,
   defineConfig({
     test: {
+      //Se pone Globals en true, para poder trabajar con la carpeta global tests
+      globals: true,
       environment: 'jsdom',
       exclude: [...configDefaults.exclude, 'e2e/**'],
       root: fileURLToPath(new URL('./', import.meta.url))
